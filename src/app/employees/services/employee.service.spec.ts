009709243b677df2cda0d8783e7a79ce4ce1a832
Arun Vijayarengan <arunprabu.v@hotmail.com>
2022-07-01 10:07:02 +0530

    updated

--- src/app/employees/services/employee.service.spec.ts
+++ src/app/employees/services/employee.service.spec.ts
@@ -1,10 +1,12 @@
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { TestBed } from '@angular/core/testing';
+import { of } from 'rxjs';
 
 import { EmployeeService } from './employee.service';
 
 describe('EmployeeService', () => {
   let service: EmployeeService;
+  let httpClientSpy: jasmine.SpyObj<HttpClient>;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
@@ -15,7 +17,43 @@ describe('EmployeeService', () => {
     service = TestBed.inject(EmployeeService);
   });
 
+  // setting up for spy 
+  beforeEach(() => {
+    httpClientSpy = jasmine.createSpyObj('HttpClient', ['get']);
+    service = new EmployeeService(httpClientSpy);
+  });
+
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  // Testing getEmployees method only 
+  it('should return expected employeeList (HttpClient called once)', (done: DoneFn) => {
+    // Mock data for employeeList
+    const mockEmployeeList = [
+      {
+        id: 1,
+        name: 'Leanne Graham',
+        phone: '1-770-736-8031 x56442'
+      },
+      {
+        id: 2,
+        name: 'Ervin Howell',
+        phone: '010-692-6593 x09125'
+      }
+    ];
+  
+    httpClientSpy.get.and.returnValue(of(mockEmployeeList));
+  
+    service.getEmployees().subscribe({
+      next: (res) => {
+        expect(res).toEqual(mockEmployeeList);
+        done();
+      },
+      error: done.fail
+    });
+    // expect(httpClientSpy.get.calls.count())
+    //   .withContext('one call')
+    //   .toBe(1);
   });
 });
